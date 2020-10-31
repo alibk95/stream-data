69956511604c201b7e9916bf82c171152f5de80e
alibk95 <abkarimi1995@gmail.com>
2020-10-31 16:18:38 +0100

    api mock added

---
+++ api-mock.py
@@ -0,0 +1,28 @@
+from flask import Flask, Response, stream_with_context
+import time
+import uuid
+import random
+
+APP = Flask(__name__)
+
+@APP.route("/", methods=["GET"])
+def home():
+    return Response("Welcome to data generator!")
+
+@APP.route("/request_data/<int:rowcount>", methods=["GET"])
+def get_large_request(rowcount):
+    """retunrs N rows of data"""
+    def f():
+        """The generator of mock data"""
+        for _i in range(rowcount):
+            time.sleep(.01)
+            txid = uuid.uuid4()
+            print(txid)
+            uid = uuid.uuid4()
+            amount = round(random.uniform(-1000, 1000), 2)
+            yield f"('{txid}', '{uid}', {amount})\n"
+    return Response(stream_with_context(f()))
+
+
+if __name__ == "__main__":
+    APP.run(debug=True)
